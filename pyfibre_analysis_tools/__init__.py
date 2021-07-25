75b43017c2a532100a77a920068b15ad4574d5bb
franklongford <franklongford@gmail.com>
2021-07-25 23:16:21 +0100

    Add ROC curve plotting for LDA Cross validation scores (#8)
    * DEV: include ROC curve plotting in new plotting module
    
    * DEV: import ROC curve plots into generic_shg_pl_trans_analysis notebook
    
    * DOC: additional documentation about ROC curve plotting
    
    * DEV: include new plotting module
    
    * STY: flake8 fix rmove import
    
    * MAINT: include dependency on sklearn
    
    * DOC: include reference to original sklearn example plot

--- pyfibre_analysis_tools/__init__.py
+++ pyfibre_analysis_tools/__init__.py
@@ -1,1 +1,2 @@
-from .analysis_tools import confidence_ellipse, scatter, load_databases  # noqa: E501
+from .analysis_tools import load_databases  # noqa: E501
+from .plotting import confidence_ellipse, scatter, plot_roc_curve, plot_lda_analysis  # noqa: E501
